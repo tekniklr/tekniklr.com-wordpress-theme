526f786b0ec4bd1e1a1853bdc5d6f3b869bbffe5
tekniklr <tekniklr@tekniklr.com>
2011-06-08 14:17:35 -0700

    Getting rid of some extraneous markup

--- page.php
+++ page.php
@@ -6,17 +6,11 @@
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+<?php the_post(); ?>
 
-				<?php the_post(); ?>
+<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php comments_template( '', true ); ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
+<?php comments_template( '', true ); ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
