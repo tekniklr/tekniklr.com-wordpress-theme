9f4afb365522c63be365914ed77f18c84ae436cd
tekniklr <tekniklr@tekniklr.com>
2011-06-10 14:45:58 -0700

    We don't need no stinkin' sidebars.

--- functions.php
+++ functions.php
@@ -203,29 +203,7 @@ endif;
  * @uses register_sidebar
  */
 function boilerplate_widgets_init() {
-	// Area 1, located at the top of the sidebar.
-	register_sidebar( array(
-		'name' => __( 'Primary Widget Area', 'boilerplate' ),
-		'id' => 'primary-widget-area',
-		'description' => __( 'The primary widget area', 'boilerplate' ),
-		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-		'after_widget' => '</li>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
-
-	// Area 2, located below the Primary Widget Area in the sidebar. Empty by default.
-	register_sidebar( array(
-		'name' => __( 'Secondary Widget Area', 'boilerplate' ),
-		'id' => 'secondary-widget-area',
-		'description' => __( 'The secondary widget area', 'boilerplate' ),
-		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-		'after_widget' => '</li>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
-
-	// Area 3, located in the footer. Empty by default.
+	// Area 1, located in the footer. Empty by default.
 	register_sidebar( array(
 		'name' => __( 'First Footer Widget Area', 'boilerplate' ),
 		'id' => 'first-footer-widget-area',
@@ -236,7 +214,7 @@ function boilerplate_widgets_init() {
 		'after_title' => '</h3>',
 	) );
 
-	// Area 4, located in the footer. Empty by default.
+	// Area 2, located in the footer. Empty by default.
 	register_sidebar( array(
 		'name' => __( 'Second Footer Widget Area', 'boilerplate' ),
 		'id' => 'second-footer-widget-area',
@@ -247,7 +225,7 @@ function boilerplate_widgets_init() {
 		'after_title' => '</h3>',
 	) );
 
-	// Area 5, located in the footer. Empty by default.
+	// Area 3, located in the footer. Empty by default.
 	register_sidebar( array(
 		'name' => __( 'Third Footer Widget Area', 'boilerplate' ),
 		'id' => 'third-footer-widget-area',
@@ -258,7 +236,7 @@ function boilerplate_widgets_init() {
 		'after_title' => '</h3>',
 	) );
 
-	// Area 6, located in the footer. Empty by default.
+	// Area 4, located in the footer. Empty by default.
 	register_sidebar( array(
 		'name' => __( 'Fourth Footer Widget Area', 'boilerplate' ),
 		'id' => 'fourth-footer-widget-area',
