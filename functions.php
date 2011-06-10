68599a84b0545c1dc509f1aeb4e052a3282bfc93
tekniklr <tekniklr@tekniklr.com>
2011-06-10 14:38:12 -0700

    Removing boilerplate admin stuf and some function overrides I don't intend on using.

--- functions.php
+++ functions.php
@@ -39,129 +39,6 @@
  */
 
 /**
- * Set the content width based on the theme's design and stylesheet.
- *
- * Used to set the width of images and content. Should be equal to the width the theme
- * is designed for, generally via the style.css stylesheet.
- */
-if ( ! isset( $content_width ) )
-	$content_width = 640;
-
-/** Tell WordPress to run boilerplate_setup() when the 'after_setup_theme' hook is run. */
-add_action( 'after_setup_theme', 'boilerplate_setup' );
-
-if ( ! function_exists( 'boilerplate_setup' ) ):
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which runs
- * before the init hook. The init hook is too late for some features, such as indicating
- * support post thumbnails.
- *
- * To override boilerplate_setup() in a child theme, add your own boilerplate_setup to your child theme's
- * functions.php file.
- *
- * @uses add_theme_support() To add support for post thumbnails and automatic feed links.
- * @uses register_nav_menus() To add support for navigation menus.
- * @uses add_custom_background() To add support for a custom background.
- * @uses add_editor_style() To style the visual editor.
- * @uses load_theme_textdomain() For translation/localization support.
- * @uses add_custom_image_header() To add support for a custom header.
- * @uses register_default_headers() To register the default custom header images provided with the theme.
- * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
- *
- * @since Twenty Ten 1.0
- */
-function boilerplate_setup() {
-
-	// This theme styles the visual editor with editor-style.css to match the theme style.
-	add_editor_style();
-
-	// Uncomment if you choose to use post thumbnails; add the_post_thumbnail() wherever thumbnail should appear
-	//add_theme_support( 'post-thumbnails' );
-
-	// Add default posts and comments RSS feed links to head
-	add_theme_support( 'automatic-feed-links' );
-
-	// Make theme available for translation
-	// Translations can be filed in the /languages/ directory
-	load_theme_textdomain( 'boilerplate', TEMPLATEPATH . '/languages' );
-
-	$locale = get_locale();
-	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Navigation', 'boilerplate' ),
-	) );
-
-	// This theme allows users to set a custom background
-	add_custom_background();
-
-	// Your changeable header business starts here
-	define( 'HEADER_TEXTCOLOR', '' );
-	// No CSS, just IMG call. The %s is a placeholder for the theme template directory URI.
-	define( 'HEADER_IMAGE', '%s/images/headers/path.jpg' );
-
-	// The height and width of your custom header. You can hook into the theme's own filters to change these values.
-	// Add a filter to boilerplate_header_image_width and boilerplate_header_image_height to change these values.
-	define( 'HEADER_IMAGE_WIDTH', apply_filters( 'boilerplate_header_image_width', 940 ) );
-	define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'boilerplate_header_image_height', 198 ) );
-
-	// We'll be using post thumbnails for custom header images on posts and pages.
-	// We want them to be 940 pixels wide by 198 pixels tall.
-	// Larger images will be auto-cropped to fit, smaller ones will be ignored. See header.php.
-	set_post_thumbnail_size( HEADER_IMAGE_WIDTH, HEADER_IMAGE_HEIGHT, true );
-
-	// Don't support text inside the header image.
-	define( 'NO_HEADER_TEXT', true );
-
-	// Add a way for the custom header to be styled in the admin panel that controls
-	// custom headers. See boilerplate_admin_header_style(), below.
-	add_custom_image_header( '', 'boilerplate_admin_header_style' );
-
-	// ... and thus ends the changeable header business.
-
-	// Default custom headers packaged with the theme. %s is a placeholder for the theme template directory URI.
-	register_default_headers( array(
-		'berries' => array(
-			'url' => '%s/images/headers/starkers.png',
-			'thumbnail_url' => '%s/images/headers/starkers-thumbnail.png',
-			/* translators: header image description */
-			'description' => __( 'Boilerplate', 'boilerplate' )
-		)
-	) );
-}
-endif;
-
-if ( ! function_exists( 'boilerplate_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * Referenced via add_custom_image_header() in boilerplate_setup().
- *
- * @since Twenty Ten 1.0
- */
-function boilerplate_admin_header_style() {
-?>
-<style type="text/css">
-/* Shows the same border as on front end */
-#headimg {
-	border-bottom: 1px solid #000;
-	border-top: 4px solid #000;
-}
-/* If NO_HEADER_TEXT is false, you would style the text with these selectors:
-	#headimg #name { }
-	#headimg #desc { }
-*/
-</style>
-<?php
-}
-endif;
-
-/**
  * Makes some changes to the <title> tag, by filtering the output of wp_title().
  *
  * If we have a site description and we're viewing the home page or a blog posts
@@ -205,11 +82,6 @@ function boilerplate_filter_wp_title( $title, $separator ) {
 	// Otherwise, let's start by adding the site name to the end:
 	$title .= get_bloginfo( 'name', 'display' );
 
-	// If we have a site description and we're on the home/front page, add the description:
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title .= " $separator " . $site_description;
-
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
 		$title .= " $separator " . sprintf( __( 'Page %s', 'boilerplate' ), max( $paged, $page ) );
@@ -220,30 +92,13 @@ function boilerplate_filter_wp_title( $title, $separator ) {
 add_filter( 'wp_title', 'boilerplate_filter_wp_title', 10, 2 );
 
 /**
- * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
- *
- * To override this in a child theme, remove the filter and optionally add
- * your own function tied to the wp_page_menu_args filter hook.
- *
- * @since Twenty Ten 1.0
- */
-function boilerplate_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
-}
-add_filter( 'wp_page_menu_args', 'boilerplate_page_menu_args' );
-
-/**
- * Sets the post excerpt length to 40 characters.
- *
- * To override this length in a child theme, remove the filter and add your own
- * function tied to the excerpt_length filter hook.
+ * Sets the post excerpt length 
  *
  * @since Twenty Ten 1.0
  * @return int
  */
 function boilerplate_excerpt_length( $length ) {
-	return 40;
+	return 140;
 }
 add_filter( 'excerpt_length', 'boilerplate_excerpt_length' );
 
@@ -287,19 +142,6 @@ function boilerplate_custom_excerpt_more( $output ) {
 	return $output;
 }
 add_filter( 'get_the_excerpt', 'boilerplate_custom_excerpt_more' );
-
-/**
- * Remove inline styles printed when the gallery shortcode is used.
- *
- * Galleries are styled by the theme in Twenty Ten's style.css.
- *
- * @since Twenty Ten 1.0
- * @return string The gallery style filter, with the styles themselves removed.
- */
-function boilerplate_remove_gallery_css( $css ) {
-	return preg_replace( "#<style type='text/css'>(.*?)</style>#s", '', $css );
-}
-add_filter( 'gallery_style', 'boilerplate_remove_gallery_css' );
 
 if ( ! function_exists( 'boilerplate_comment' ) ) :
 /**
@@ -493,10 +335,6 @@ function boilerplate_posted_in() {
 	);
 }
 endif;
-
-/*	Begin Boilerplate */
-	// Add Admin
-		require_once(TEMPLATEPATH . '/boilerplate-admin/admin-menu.php');
 
 	// remove version info from head and feeds (http://digwp.com/2009/07/remove-wordpress-version-number/)
 		function complete_version_removal() {
