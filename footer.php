f2c0311ea6ce67e2b0e83843d6001da2d782b824
tekniklr <tekniklr@tekniklr.com>
2011-06-14 16:08:19 -0700

    HTML tag fix. Don't want to use no-js.

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
-</div>
-<?php $footer = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/footer"); echo $footer; ?>
-</div>
-<?php $pageend = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/pageend"); echo $pageend; ?>
-</body>
+      </div>
+        <?php $footer = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/footer"); echo $footer; ?>
+    </div>
+    <?php $pageend = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/pageend"); echo $pageend; ?>
+  </body>
+</html>
--- header.php
+++ header.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html> 
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]--> 
-<!--[if IE 7 ]> <html lang="en" class="no-js ie7"> <![endif]--> 
-<!--[if IE 8 ]> <html lang="en" class="no-js ie8"> <![endif]--> 
-<!--[if IE 9 ]> <html lang="en" class="no-js ie9"> <![endif]--> 
-<!--[if gt IE 9]> <html lang="en" class="no-js"> <![endif]--> 
-<head>
-  <?php $headincmeta = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/headincmeta"); echo $headincmeta; ?>
-  <title><?php
-	  global $page, $paged;
-	  $title = get_the_title();
-	  echo strtolower($title);
-  ?> ~ blog ~ tekniklr.com</title>
-  <?php wp_head(); ?>
-</head>
+<html lang="en">
+  <head>
+    <?php $headincmeta = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/headincmeta"); echo $headincmeta; ?>
+    <title><?php
+  	  global $page, $paged;
+  	  $title = get_the_title();
+  	  echo strtolower($title);
+    ?> ~ blog ~ tekniklr.com</title>
+    <?php wp_head(); ?>
+  </head>
 
-<body>
-  <div id="container">
-    <?php $header = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/header"); echo $header; ?>
-    <?php $navigation = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/navigation"); echo $navigation; ?>
-    <div id="main">
+  <body>
+    <div id="container">
+      <?php $header = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/header"); echo $header; ?>
+      <?php $navigation = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/navigation"); echo $navigation; ?>
+      <div id="main">
