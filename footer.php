082ccbf3e2d2cb49ce25dd8b97c3648fdfbd86d8
tekniklr <tekniklr@tekniklr.com>
2011-06-16 21:37:21 -0700

    Wasn't calling wp_footer() and that's why the admin bar wasn't showing. Durr.

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
         <?php $footer = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/footer"); echo $footer; ?>
     </div>
     <?php $pageend = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/pageend"); echo $pageend; ?>
+    <?php wp_footer(); ?>
   </body>
 </html>
