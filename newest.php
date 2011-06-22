628037a70ddedfc5a159e488d2770d8803291dea
tekniklr <tekniklr@tekniklr.com>
2011-06-22 14:58:17 -0700

    Added script to be run in the most hinky way possible by rails to display the most recent wordpress blog post on the rails index without making any HTTP calls.

---
+++ newest.php
@@ -0,0 +1,36 @@
+<?php
+
+include('public/wpblog/wp-blog-header.php');
+
+$num_shown = 0;
+$num_to_show = 1;
+
+if ( have_posts() ) while ( have_posts() && ($num_shown < $num_to_show) ) : the_post(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<h1><?php the_title(); ?></h1>
+	<div class="entry-meta">
+		<?php boilerplate_posted_on(); ?>
+	</div><!-- .entry-meta -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+	</div><!-- .entry-content -->
+<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+	<footer id="entry-author-info">
+		<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'boilerplate_author_bio_avatar_size', 60 ) ); ?>
+		<h2><?php printf( esc_attr__( 'About %s', 'boilerplate' ), get_the_author() ); ?></h2>
+		<?php the_author_meta( 'description' ); ?>
+		<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+			<?php printf( __( 'View all posts by %s &rarr;', 'boilerplate' ), get_the_author() ); ?>
+		</a>
+	</footer><!-- #entry-author-info -->
+<?php endif; ?>
+	<footer class="entry-utility">
+		<?php boilerplate_posted_in(); ?>
+		<?php edit_post_link( __( 'Edit', 'boilerplate' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-utility -->
+</article><!-- #post-## -->
+
+<?php $num_shown++; ?>
+<?php endwhile; // end of the loop. ?>
