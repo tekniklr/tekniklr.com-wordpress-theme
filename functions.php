d994114dc5de5bfcb77020fe0d74e1207a22e121
tekniklr <tekniklr@tekniklr.com>
2011-06-22 20:20:25 -0700

    Why the hell would I have a 'bookmark this' link ANYWHERE.

--- functions.php
+++ functions.php
@@ -283,19 +283,17 @@ function boilerplate_posted_in() {
 	// Retrieves tag list of current post, separated by commas.
 	$tag_list = get_the_tag_list( '', ', ' );
 	if ( $tag_list ) {
-		$posted_in = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'boilerplate' );
+		$posted_in = __( 'This entry was posted in %1$s and tagged %2$s.', 'boilerplate' );
 	} elseif ( is_object_in_taxonomy( get_post_type(), 'category' ) ) {
-		$posted_in = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'boilerplate' );
+		$posted_in = __( 'This entry was posted in %1$s.', 'boilerplate' );
 	} else {
-		$posted_in = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'boilerplate' );
+		$posted_in = __( '', 'boilerplate' );
 	}
 	// Prints the string, replacing the placeholders.
 	printf(
 		$posted_in,
 		get_the_category_list( ', ' ),
-		$tag_list,
-		get_permalink(),
-		the_title_attribute( 'echo=0' )
+		$tag_list
 	);
 }
 endif;
