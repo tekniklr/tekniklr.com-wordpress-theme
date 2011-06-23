31ff4783b47afc9321cdbba666571265d4589ccc
tekniklr <tekniklr@tekniklr.com>
2011-06-22 22:55:35 -0700

    Don't want to show title on blog pages.

--- page.php
+++ page.php
@@ -15,11 +15,6 @@
 get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php if ( is_front_page() ) { ?>
-					<h2 class="entry-title"><?php the_title(); ?></h2>
-				<?php } else { ?>	
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php } ?>
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
