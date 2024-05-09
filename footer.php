22218104f561f831bf7ddc284abd2c6852540994
tekniklr <tekniklr@tekniklr.com>
2024-05-09 11:45:30 -0700

    update paths so wordpress will run on a separate subdomain

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
         </div>
       </div>
-      <?php $footer = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/footer"); echo $footer; ?>
+      <?php $footer = file_get_contents("https://tekniklr.com/static/footer"); echo $footer; ?>
     </div>
-    <?php $pageend = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/pageend"); echo $pageend; ?>
+    <?php $pageend = file_get_contents("https://tekniklr.com/static/pageend"); echo $pageend; ?>
     <?php wp_footer(); ?>
   </body>
 </html>
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> 
 <html lang="en">
   <head>
-    <?php $headincmeta = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/headincmeta"); echo $headincmeta; ?>
+    <?php $headincmeta = file_get_contents("https://tekniklr.com/static/headincmeta"); echo $headincmeta; ?>
     <title><?php
   	  global $page, $paged;
   	  $title = wp_title(' ☆ ', false, 'right');
@@ -14,7 +14,7 @@
 
   <body onload="prettyPrint()">
     <div id="container">
-      <?php $header = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/header"); echo $header; ?>
-      <?php $navigation = file_get_contents("http://".$_SERVER['HTTP_HOST']."/static/navigation"); echo $navigation; ?>
+      <?php $header = file_get_contents("https://tekniklr.com/static/header"); echo $header; ?>
+      <?php $navigation = file_get_contents("https://tekniklr.com/static/navigation"); echo $navigation; ?>
       <div id="main">
       	<div id="wordpress">
