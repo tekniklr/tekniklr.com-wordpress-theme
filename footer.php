17cf78a8ba636da9ec49881da883f5e8b2093258
tekniklr <tekniklr@tekniklr.com>
2024-06-06 10:14:01 -0700

    don't show homepage footer on wordpress
    none of it was ever viewable given the infinite scroll, and when scrolling very quickly sometimes graphical artefacts were left behind in posts

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
         </div>
       </div>
-      <?php $footer = file_get_contents("https://tekniklr.com/static/footer"); echo $footer; ?>
     </div>
     <?php $pageend = file_get_contents("https://tekniklr.com/static/pageend"); echo $pageend; ?>
     <?php wp_footer(); ?>
